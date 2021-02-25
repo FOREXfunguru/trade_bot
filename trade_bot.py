cf8c7ed986599b94cec4dd168bbb4fe6b858bed4
elowy01 <ernestolowy@gmail.com>
2021-02-25 23:34:56 +0000

    Changes

--- trade_bot.py
+++ trade_bot.py
@@ -30,115 +30,13 @@ class TradeBot(object):
     pair: str, Required
           Currency pair used in the trade. i.e. AUD_USD
     timeframe: str, Required
-               Timeframe used for the trade. Possible values are: D,H12,H10,H8,H4
+               Timeframe used for the trade. Possible values are: D,H12,H10,H8,H4,H1
     '''
     def __init__(self, start, end, pair, timeframe):
         self.start = start
         self.end = end
         self.pair = pair
         self.timeframe = timeframe
-
-    def adjust_SL(self, type, clObj, number=7):
-        '''
-        Function to adjust the SL price
-        to the most recent highest high/lowest low
-
-        Parameters
-        ----------
-        type : str
-               Trade type ('long'/ 'short')
-        clObj : CandleList obj
-        number : int
-                 Number of candles to go back
-                 to adjust the SL. Default: 7
-
-        Returns
-        -------
-        float: adjusted SL
-        '''
-
-        if type == 'short':
-            part = 'high{0}'.format(CONFIG.get('general', 'bit'))
-        elif type == 'long':
-            part = 'low{0}'.format(CONFIG.get('general', 'bit'))
-
-        SL = None
-        ix = 0
-        for c in reversed(clObj.data['candles']):
-            # go back 'number' candles
-            if ix == number:
-                break
-            ix += 1
-            price = c[part]
-            if SL is None:
-                SL = price
-                continue
-            if type == 'short':
-                if price > SL:
-                    SL = price
-            if type == 'long':
-                if price < SL:
-                    SL = price
-
-        return SL
-
-    def prepare_trade(self, type, SL, ic, harea_sel, delta,
-                      add_pips):
-        '''
-        Prepare a Trade object
-        and check if it is taken
-
-        Parameters
-        ----------
-        type : str,
-               Type of trade. 'short' or 'long'
-        SL : float,
-             Adjusted (by '__get_trade_type') SL price
-        ic : Candle object
-             Indecision candle for this trade
-        harea_sel : HArea of this trade
-        delta : Timedelta object corresponding to
-                the time that needs to be increased
-        add_pips : Number of pips above/below SL and entry
-                   price to consider for recalculating
-                   the SL and entry. Default : None
-
-        Returns
-        -------
-        Trade object
-        '''
-        startO = ic.time + delta
-        if type == 'short':
-            # entry price will be the low of IC
-            entry_p = getattr(ic, "low{0}".format(CONFIG.get('general', 'bit')))
-            if add_pips is not None:
-                SL = round(add_pips2price(self.pair,
-                                          SL, add_pips), 4)
-                entry_p = round(substract_pips2price(self.pair,
-                                                     entry_p, add_pips), 4)
-        elif type == 'long':
-            # entry price will be the high of IC
-            entry_p = getattr(ic, "high{0}".format(CONFIG.get('general', 'bit')))
-            if add_pips is not None:
-                entry_p = add_pips2price(self.pair,
-                                    entry_p, add_pips)
-                SL = substract_pips2price(self.pair,
-                                          SL, add_pips)
-
-        startO = ic.time+delta
-        t = Trade(
-            id='{0}.bot'.format(self.pair),
-            start=startO.strftime('%Y-%m-%d %H:%M:%S'),
-            pair=self.pair,
-            timeframe=self.timeframe,
-            type=type,
-            entry=entry_p,
-            SR=harea_sel.price,
-            SL=SL,
-            RR=CONFIG.getfloat('trade_bot', 'RR'),
-            strat='counter')
-
-        return t
 
     def run(self, discard_sat=True):
         '''
@@ -283,7 +181,8 @@ class TradeBot(object):
                     prepare_trade = False
 
                 if prepare_trade is True:
-                    t = self.prepare_trade(
+                    t = prepare_trade(
+                        t_bot=self,
                         type=type,
                         SL=SL,
                         ic=c_candle,
@@ -303,3 +202,108 @@ class TradeBot(object):
             return None
         else:
             return tlist
+
+class TradeDiscover(object):
+    '''
+    This class represents an automatic Trading bot to discover new trades
+
+    Class variables
+    ---------------
+    now : datetime
+    pair: str, Required
+          Currency pair used in the trade. i.e. AUD_USD
+    timeframe: str, Required
+               Timeframe used for the trade. Possible values are: D,H12,H10,H8,H4
+    '''
+    def __init__(self, pair, timeframe):
+        self.now = date.today()
+        self.pair = pair
+        self.timeframe = timeframe
+
+    def run(self):
+        """
+        Run the bot
+        """
+        tb_logger.info("Running...")
+
+        conn = Connect(instrument=self.pair,
+                       granularity=self.timeframe)
+
+        ser_dir = None
+        if CONFIG.has_option('general', 'ser_data_dir'):
+            ser_dir = CONFIG.get('general', 'ser_data_dir')
+
+        delta = nhours = None
+        if self.timeframe == "D":
+            nhours = 24
+            delta = timedelta(hours=24)
+        else:
+            p1 = re.compile('^H')
+            m1 = p1.match(self.timeframe)
+            if m1:
+                nhours = int(self.timeframe.replace('H', ''))
+                delta = timedelta(hours=int(nhours))
+
+        # calculate the start datetime for the CList that will be used
+        # for calculating the S/R areas
+        delta_period = periodToDelta(CONFIG.getint('trade_bot', 'period_range'),
+                                     self.timeframe)
+
+        initc_date = self.now - delta_period
+        # Get now a CandleList from 'initc_date' to 'startO' which is the
+        # total time interval for this TradeBot
+        res = conn.query(start=initc_date.isoformat(),
+                         end=self.now.isoformat(),
+                         indir=ser_dir)
+        clO = CandleList(res)
+        dt_str = self.now.strftime("%d_%m_%Y_%H_%M")
+        outfile_png = "{0}/srareas/{1}.{2}.{3}.halist.png".format(CONFIG.get("images", "outdir"),
+                                                                  self.pair, self.timeframe, dt_str)
+        SRlst = calc_SR(clO, outfile=outfile_png)
+
+        # fetch candle for current datetime
+        res = conn.query(start=self.now.isoformat(),
+                         count=1,
+                         indir=ser_dir)
+
+        # this is the current candle that
+        # is being checked
+        c_candle = Candle(dict_data=res['candles'][0])
+        c_candle.time = datetime.strptime(c_candle.time,
+                                          '%Y-%m-%dT%H:%M:%S.%fZ')
+
+        # check if there is any HArea overlapping with c_candle
+        HAreaSel, sel_ix = SRlst.onArea(candle=c_candle)
+
+        if HAreaSel is not None:
+            c_candle.set_candle_features()
+            # guess the if trade is 'long' or 'short'
+            newCl = clO.slice(start=initc_date, end=c_candle.time)
+            type = get_trade_type(c_candle.time, newCl)
+            SL = self.adjust_SL(type, newCl)
+            prepare_trade = False
+            if c_candle.indecision_c(ic_perc=CONFIG.getint('general', 'ic_perc')) is True:
+                prepare_trade = True
+            elif type == 'short' and c_candle.colour == 'red':
+                prepare_trade = True
+            elif type == 'long' and c_candle.colour == 'green':
+                prepare_trade = True
+
+            # discard if IC falls on a Saturday
+            if c_candle.time.weekday() == 5 and discard_sat is True:
+                tb_logger.info("Possible trade at {0} falls on Sat. Skipping...".format(c_candle.time))
+                prepare_trade = False
+
+            if prepare_trade is True:
+                t = self.prepare_trade(
+                    type=type,
+                    SL=SL,
+                    ic=c_candle,
+                    harea_sel=HAreaSel,
+                    delta=delta,
+                    add_pips=CONFIG.getint('trade', 'add_pips'))
+                t.tot_SR = len(SRlst.halist)
+                t.rank_selSR = sel_ix
+                t.SRlst = SRlst
+
+    tb_logger.info("Run done")
